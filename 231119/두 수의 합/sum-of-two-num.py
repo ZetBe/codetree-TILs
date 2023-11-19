621544b56290fdb5e9414591330f92e2a0654920
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-19 11:16:12 +0900

    Title: 두 수의 합, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231119/두 수의 합/sum-of-two-num.py
+++ 231119/두 수의 합/sum-of-two-num.py
@@ -3,9 +3,10 @@ arr = list(map(int, input().split()))
 arr.sort()
 c = 0
 l, r = 0, n-1
-while l != r:
+while l < r:
     if arr[l] + arr[r] == k:
         l += 1
+        r -= 1
         c += 1
     elif arr[l] + arr[r] > k:
         r -= 1

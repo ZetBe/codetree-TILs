f54c95abc57eb9b74d9266e2f9a7c7f3fc82c61a
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-24 18:02:10 +0900

    Title: M개의 구간을 선택하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231124/M개의 구간을 선택하기/select-m-sections.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+dp = [[0 for j in range(m)] for i in range(n)]
+dp[0][0] = arr[0]
+dp[n-1][m-1] = arr[n-1]
+for i in range(1, n):
+    dp[i][0] += arr[i]+dp[i-1][0]
+
+for j in range(1, m):
+    for i in range(2, n):
+        for k in range(i-1):
+            dp[i][j] = max(dp[i][j], dp[i-2-k][j-1]+arr[i], dp[i-1][j]+arr[i])
+ma = 0
+for i in dp:
+    ma = max(ma, max(i))
+print(ma)

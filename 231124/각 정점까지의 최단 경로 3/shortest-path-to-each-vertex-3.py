7b688f1debc812a254684e57825e2e80d4853a51
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-24 14:47:31 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231124/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+graph = [[] for i in range(n+1)]
+dijk = [0 for i in range(n+1)]
+v = [0 for i in range(n+1)]
+for i in range(m):
+    t, f, g = map(int, input().split())
+    graph[t].append([f, g])
+def dijkstra(n, now):
+    if dijk[n] == 0:
+        dijk[n] = now
+    else:
+        dijk[n] = min(now, dijk[n])
+    if v[n] == 1:
+        return
+    v[n] = 1
+    for i in graph[n]:
+        dijkstra(i[0], now+i[1])
+
+v[1] = 1
+for i in graph[1]:
+    dijkstra(i[0], i[1])
+
+if n == 1:
+    print(0)
+else:
+    for i in range(2, len(dijk)):
+        print(dijk[i])

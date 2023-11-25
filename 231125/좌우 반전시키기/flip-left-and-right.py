0761c8026a1b401f31b466cf146854cc90e83cc8
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-25 21:52:25 +0900

    Title: 좌우 반전시키기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231125/좌우 반전시키기/flip-left-and-right.py
+++ 231125/좌우 반전시키기/flip-left-and-right.py
@@ -1,8 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
 a = 0
-for i in range(1, n):
+for i in range(1, n-1):
     if arr[i-1] == 0:
         arr[i-1], arr[i], arr[i+1] = (arr[i-1]+1)%2, (arr[i]+1)%2, (arr[i+1]+1)%2
         a += 1
-print(a)
+if arr[n-1] == 0:
+    arr[n-2], arr[n-1] = (arr[n-2]+1)%2, (arr[n-1]+1)%2
+if sum(arr) == n:
+    print(a)
+else:
+    print(-1)

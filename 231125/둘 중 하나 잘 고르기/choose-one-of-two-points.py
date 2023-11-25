35ec1d4528d9114a1ad74bb291ba85d6cf405d56
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-25 21:30:28 +0900

    Title: 둘 중 하나 잘 고르기, Time: 195ms, Memory: 25MB, Status: Passed - CodeTree

--- 231125/둘 중 하나 잘 고르기/choose-one-of-two-points.py
+++ 231125/둘 중 하나 잘 고르기/choose-one-of-two-points.py
@@ -4,7 +4,7 @@ dp = [[0 for i in range(n*2)] for j in range(n*2)]
 
 dp[0][0], dp[0][1] = arr[0][0], arr[0][1]
 for i in range(1, n*2):
-    for j in range(i+1):
+    for j in range(i+2):
         if j < n*2:
             dp[i][j] = max(dp[i][j], dp[i-1][j]+arr[i][0], dp[i-1][j-1]+arr[i][1])
 print(dp[n*2-1][n])

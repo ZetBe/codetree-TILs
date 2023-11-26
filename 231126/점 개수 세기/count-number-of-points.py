4fc0b0ef52dde7ac37c3bb29a728678faef87cb8
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-26 12:51:52 +0900

    Title: 점 개수 세기, Time: 826ms, Memory: 30MB, Status: Passed - CodeTree

--- 231126/점 개수 세기/count-number-of-points.py
+++ 231126/점 개수 세기/count-number-of-points.py
@@ -1,6 +1,7 @@
 import bisect
 n, q = map(int, input().split())
 arr = list(map(int, input().split()))
+arr.sort()
 for i in range(q):
     a, b = map(int, input().split())
     print(bisect.bisect_right(arr, b)-bisect.bisect_left(arr, a))

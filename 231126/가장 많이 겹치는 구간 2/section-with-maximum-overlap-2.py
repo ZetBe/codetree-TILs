01bae827670ebf325de4bd494755c3e4429f8ace
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-11-26 17:54:21 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231126/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.py
@@ -0,0 +1,23 @@
+import heapq
+x1, x2 = [], [] 
+n = int(input())
+line = [list(map(int, input().split())) for i in range(n)]
+line.sort()
+for i in range(n):
+    heapq.heappush(x1, line[i][0])
+    heapq.heappush(x2, line[i][1])
+a = 1
+answer = 0
+heapq.heappop(x1)
+for i in range(n*2-1):
+    if len(x1) == 0:
+        break
+    if x1[0] <= x2[0]:
+        a += 1
+        answer = max(answer, a)
+        heapq.heappop(x1)
+    else:
+        a -= 1
+        heapq.heappop(x2)
+
+print(answer)

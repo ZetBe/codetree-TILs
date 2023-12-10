4df72cafd6a8693913beb17e902f669ca0bc38dc
Heewon Seo <seoheewon123@hufs.ac.kr>
2023-12-10 16:44:18 +0900

    Title: 삼 오 무, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231210/삼 오 무/three-five-moo.py
@@ -0,0 +1,19 @@
+n = int(input())
+l, r = 1, n*2
+
+while l <= r:
+    mid = (l+r)//2
+    if mid%3==0 or mid%5==0:
+        l+=2
+    else:
+        three = mid//3
+        five = mid//5
+        tf = mid//15
+        total = mid-(three+five-tf)
+        if total == n:
+            break
+        elif total < n:
+            l = mid+1
+        else:
+            r = mid-1
+print(mid)

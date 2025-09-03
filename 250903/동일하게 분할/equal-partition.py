f64dcc58efc2a89272030793d29569afa5aa8d86
Heewon Seo <seoheewon123@hufs.ac.kr>
2025-09-03 12:00:56 +0900

    Title: 동일하게 분할, Time: 175ms, Memory: 25MB, Status: Passed - Codetree

--- 250903/동일하게 분할/equal-partition.py
+++ 250903/동일하게 분할/equal-partition.py
@@ -1,15 +1,24 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
+arr.sort()
 # Please write your code here.
-dp = [0 for i in range(sum(arr)+1)]
-for i in arr:
-    dp[i] = 1
+dp = [[] for i in range(sum(arr)+1)]
+for i in range(n):
+    if len(dp[arr[i]]) == 0:
+        dp[arr[i]].append(i)
 for i in range(sum(arr)):
-    for j in arr:
-        if i+j < n:
-            dp[i+j] = 1
-if dp[sum(arr)//2] == 1:
-    print('Yes')
-else:
+    for j in range(n):
+        if i-arr[j] >= 0 and len(dp[i-arr[j]]) > 0 and j not in dp[i-arr[j]]:
+            temp = 0
+            for k in dp[i]:
+                temp += arr[k]
+            if temp < i:
+                dp[i] = dp[i-arr[j]][:]
+                dp[i].append(j)
+if sum(arr) % 2 == 1:
     print('No')
+else:
+    if len(dp[sum(arr)//2]) > 0:
+        print('Yes')
+    else:
+        print('No')
